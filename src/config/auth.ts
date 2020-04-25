12250f435328135fb774d3286cbb519839a6ce18
pcampina <pablo.dsn@gmail.com>
2020-04-25 18:29:41 -0300

    upload finish

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '69c8b2682cff47b91c4d933622985e6b',
+    expiresIn: '1d',
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+
+import express from 'express';
+import routes from './routes';
+import uploadConfig from './config/upload';
+
+import './database';
+import upload from './config/upload';
+
+const app = express();
+
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🟢 Server started on port 3333');
+});
